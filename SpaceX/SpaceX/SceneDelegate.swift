43fdb17269de0a20285103f9f54af830684e607a
Эван Крошкин <>
2022-08-29 18:55:29 +0300

    Initial project setup

---
+++ SpaceX/SpaceX/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  SpaceX
+//
+//  Created by Эван Крошкин on 29.08.22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
